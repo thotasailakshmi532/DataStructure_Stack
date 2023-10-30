7fe37db2cb1e94eb3d2996c73e2701dc984f4fca
thotasailakshmi532 <thotasailakshmi532@gmail.com>
2023-10-30 16:24:21 +0530

    Add project files.

---
+++ DataStructure_Stack/Program.cs
@@ -0,0 +1,44 @@
+﻿using StackLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DataStructure_Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            runApp();
+            Console.ReadLine();
+        }
+
+        public static void runApp()
+        {
+            Stack stack = new Stack();
+
+            Console.WriteLine("length of stack=" + stack.getLengthOfStack());
+            if (stack.peek() == null)
+                Console.WriteLine("Stack contains nothing");
+            for (int i = 0; i < 10; i++)
+            {
+                Node n = new Node();
+                n.Data = i.ToString();
+                stack.push(n);
+                Console.WriteLine("Length of stack=" + stack.getLengthOfStack());
+            }
+
+            Node node = stack.pop();
+            Console.WriteLine("Popped " + node.Data);
+
+            node = stack.pop();
+            Console.WriteLine("Popped " + node.Data);
+
+            Console.WriteLine(stack.listContents());
+        }
+    }
+}
